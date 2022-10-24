ece9a8eca3103e18ba9aa598dec3784a8b5556f1
Kairzhan Muldashev <kair8m@gmail.com>
2022-10-24 02:03:51 +0200

    minor improvements in packer

--- init.lua
+++ init.lua
@@ -8,9 +8,16 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 0
 vim.opt.expandtab = true
 vim.opt.swapfile = false
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard:append('unnamedplus')
 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = 'yes'
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.iskeyword:append('-')
 
 vim.api.nvim_set_keymap('n', '<ESC>', '<CMD>noh<CR>', {silent = true})
 
--- lua/packer-init.lua
+++ lua/packer-init.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim' -- Packagemanager
@@ -89,4 +102,9 @@ return require('packer').startup(function(use)
         'kylechui/nvim-surround',
         rag = '*'
     }
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
+
