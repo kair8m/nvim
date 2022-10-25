5a3796480f86f221b195a0abaf4bd90fc134a8ed
Kair <kair8m@gmail.com>
2022-10-25 09:57:40 +0000

    add relative path to lualine

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -1,1 +1,14 @@
-require('lualine').setup()
+require('lualine').setup({
+    options = { theme = 'dracula' },
+    sections = {
+        lualine_a = {
+            {
+                'filename',
+                file_status = true, -- displays file status (readonly status, modified status)
+                path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        }
+
+    }
+})
+
