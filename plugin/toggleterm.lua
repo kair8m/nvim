8ec7cee6dd017d5cb7aebdff8a575a373f2bad24
Kair <kair8m@gmail.com>
2022-10-31 14:54:18 +0000

    add protected call for toggleterm plugin

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -1,4 +1,8 @@
-require('toggleterm').setup({
+local status_ok, toggleterm = pcall(require, 'toggleterm')
+if not status_ok then
+    return
+end
+toggleterm.setup({
     size = 20,
     open_mapping = [[<C-\>]],
     hide_numbers = true,
