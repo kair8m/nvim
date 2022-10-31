cd69bf4f506b339f172a978f18726d1003a0688d
Kair <kair8m@gmail.com>
2022-10-31 14:01:56 +0000

    add protected call for fidget plugin

--- plugin/fidget.lua
+++ plugin/fidget.lua
@@ -1,4 +1,9 @@
-require('fidget').setup({
+local status_ok, fidget = pcall(require, 'fidget')
+if not status_ok then
+    return
+end
+
+fidget.setup({
     window = {
         relative = "win", -- where to anchor, either "win" or "editor"
         blend = 0, -- &winblend for the window
