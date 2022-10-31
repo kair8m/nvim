856f26e1192934b01550c85702b8446081911fa5
Kair <kair8m@gmail.com>
2022-10-31 14:35:03 +0000

    add protected call for lsp-colors plugin

--- plugin/lsp-colors.lua
+++ plugin/lsp-colors.lua
@@ -1,5 +1,9 @@
--- Lua
-require("lsp-colors").setup({
+local status_ok, lsp_colors = pcall(require, "lsp-colors")
+if not status_ok then
+    return
+end
+
+lsp_colors.setup({
   Error = "#db4b4b",
   Warning = "#e0af68",
   Information = "#0db9d7",
