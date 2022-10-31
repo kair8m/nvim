c92b16a3a5714aa1ef38c0348e5afc7153881518
Kair <kair8m@gmail.com>
2022-10-31 14:00:22 +0000

    add protected call for cursorline plugin

--- plugin/cursorline.lua
+++ plugin/cursorline.lua
@@ -1,4 +1,9 @@
-require('nvim-cursorline').setup {
+local status_ok, cursorline = pcall(require, 'nvim-cursorline')
+if not status_ok then
+    return
+end
+
+cursorline.setup {
     cursorline = {
         enable = true,
         timeout = 10,
