429b98509dfe7b490b05a26a886fe16b9a485b4d
Kair <kair8m@gmail.com>
2022-10-31 14:41:56 +0000

    add protected call for lualine plugin

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -1,4 +1,8 @@
-require('lualine').setup({
+local status_ok, lualine = pcall(require, 'lualine')
+if not status_ok then
+    return
+end
+lualine.setup({
     options = { theme = 'dracula' },
     sections = {
         lualine_a = {
