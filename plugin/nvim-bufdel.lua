ca81f9e911debe4f17ea95ee4afbfa713069113f
Kair <kair8m@gmail.com>
2022-10-31 14:45:51 +0000

    add protected call for bufdel plugin

--- plugin/nvim-bufdel.lua
+++ plugin/nvim-bufdel.lua
@@ -1,7 +1,10 @@
-vim.api.nvim_set_keymap('n', '<A-c>', '<CMD>BufDel<CR>', { silent = true })
-
-require('bufdel').setup {
+local status_ok, bufdel = pcall(require, 'bufdel')
+if not status_ok then
+    return
+end
+bufdel.setup {
     next = 'tabs', -- or 'cycle, 'alternate'
     quit = true, -- quit Neovim when last buffer is closed
 }
+vim.api.nvim_set_keymap('n', '<A-c>', '<CMD>BufDel<CR>', { silent = true })
 
