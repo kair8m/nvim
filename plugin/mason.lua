c85120989b986b1024915f962cbebee81936632f
Kair <kair8m@gmail.com>
2022-10-31 14:43:11 +0000

    add protected call for mason plugin

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,4 +1,8 @@
-require('mason').setup({
+local status_ok, mason = pcall(require, 'mason')
+if not status_ok then
+    return
+end
+mason.setup({
     ui = {
         icons = {
             package_installed = "✓",
