270d0d64c8a06aca83f53a122ca083fea4606a53
Kairzhan Muldashev <kair8m@gmail.com>
2022-11-02 15:34:37 +0100

    enable definition for lspkind

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -42,11 +42,9 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     formatting = {
-        fields = { 'menu', 'abbr', 'kind' },
         format = lspkind.cmp_format({
-            mode = 'symbol', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            maxwidth = 50,
+            ellipsis_char = '...',
         })
     },
 
