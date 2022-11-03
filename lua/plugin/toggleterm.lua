fb5cec8111ca207d6c8cd70b139d7254b53658d0
Kairzhan Muldashev <kair8m@gmail.com>
2022-11-03 01:42:47 +0100

    add lazygit

--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -16,3 +16,28 @@ toggleterm.setup({
     shell = vim.o.shell
 })
 
+local toggleterm_status, toggleterm_api = pcall(require, 'toggleterm.terminal')
+if not toggleterm_status then
+    return
+end
+
+local Terminal = toggleterm_api.Terminal
+local lazygit  = Terminal:new({
+    cmd = 'lazygit',
+    dir = 'git_dir',
+    direction = 'float',
+    float_opts = {
+        border = 'double',
+    },
+    on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+    end,
+    on_close = function(term)
+        vim.cmd("startinsert!")
+    end,
+})
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
