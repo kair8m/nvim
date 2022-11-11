6f63e1b1068699fc0c2b44f06ba8cad8fca82c1e
Kairzhan Muldashev <kair8m@gmail.com>
2022-11-11 23:33:41 +0100

    add lsp keybindings with leader prefix

--- lua/plugin/lsp/lspconfig.lua
+++ lua/plugin/lsp/lspconfig.lua
@@ -17,6 +17,55 @@ local navic_staus, navic = pcall(require, 'nvim-navic')
 if not navic_staus then
     return
 end
+local show_references_command = '<cmd>Lspsaga lsp_finder<cr>';
+local peek_definition_command = '<cmd>Lspsaga peek_definition<cr>'
+local show_declaration_command = '<cmd>lua vim.lsp.buf.declaration()<cr>'
+local show_implementation_command = '<cmd>lua vim.lsp.buf.implementation()<cr>'
+local show_type_definition_command = '<cmd>lua vim.lsp.buf.type_definition()<cr>'
+local code_actions_command = '<cmd>Lspsaga code_action<cr>'
+local rename_command = '<cmd>Lspsaga rename<cr>'
+local hover_doc_command = '<cmd>Lspsaga hover_doc<cr>'
+
+local show_diagnostics_command = '<cmd>TroubleRefresh<cr><cmd>TroubleToggle<cr>'
+local show_line_diagnostics = '<cmd>Lspsaga show_line_diagnostics<cr>'
+local show_cursor_diagnostics = '<cmd>Lspsaga show_cursor_diagnostics<cr>'
+local jump_to_next_diagnostics_finding = '<cmd>Lspsaga diagnostic_jump_next<cr>'
+local jump_to_prev_diagnostics_finding = '<cmd>Lspsaga diagnostic_jump_prev<cr>'
+local jump_to_next_error = function ()
+    require('lspsaga.diagnostic').goto_next({
+        severity = vim.diagnostic.severity.ERROR
+    })
+end
+local jump_to_prev_error = function ()
+    require('lspsaga.diagnostic').goto_prev({
+        severity = vim.diagnostic.severity.ERROR
+    })
+end
+
+local lsp_mappings_whichkey = {
+    l = {
+        name = 'LSP',
+        g = {
+            name = 'Go to...',
+            r = { show_references_command, 'Show references' },
+            d = { peek_definition_command, 'Peek definition' },
+            D = { show_declaration_command, 'Show declaration' },
+            i = { show_implementation_command, 'Show implementation' },
+            y = { show_type_definition_command, 'Show type definition' }
+        },
+        r = { rename_command, 'Rename' },
+        a = { code_actions_command, 'Code actions' },
+        k = { hover_doc_command, 'Show documentation' }
+    },
+    d = {
+        name = 'LSP Diagnostics',
+        a = { show_diagnostics_command, 'Toggle diagnostics results' },
+        l = { show_line_diagnostics, 'Show line diagnostics' },
+        c = { show_cursor_diagnostics, 'Show cursor diagnostics' },
+        n = { jump_to_next_error, 'Jump to next error' },
+        p = { jump_to_prev_error, 'Jump to previous error' }
+    }
+}
 
 local on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -28,29 +77,27 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
 
     -- Displays hover information about the symbol under the cursor
-    bufmap('n', 'K', '<cmd>Lspsaga hover_doc<cr>')
+    bufmap('n', 'K', hover_doc_command)
     -- Jump to the definition
-    bufmap('n', 'gd', '<cmd>Lspsaga peek_definition<cr>')
+    bufmap('n', 'gd', peek_definition_command)
     -- Jump to declaration
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    bufmap('n', 'gD',show_declaration_command)
     -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    bufmap('n', 'gi', show_implementation_command)
     -- Jumps to the definition of the type symbol
-    bufmap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    bufmap('n', 'gy', show_type_definition_command)
     -- Lists all the references
-    bufmap('n', 'gr', '<cmd>Lspsaga lsp_finder<cr>')
-    -- Displays a function's signature information
-    -- bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    bufmap('n', 'gr', show_references_command)
     -- Renames all references to the symbol under the cursor
-    bufmap('n', 'rn', '<cmd>Lspsaga rename<cr>')
+    bufmap('n', 'rn', rename_command)
     -- Selects a code action available at the current cursor position
-    bufmap('n', '<C-a>', '<cmd>Lspsaga code_action<cr>')
+    bufmap('n', '<C-a>', code_actions_command)
     -- Show diagnostics in a floating window
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+    bufmap('n', 'gl', show_line_diagnostics)
     -- Move to the previous diagnostic
-    bufmap('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<cr>')
+    bufmap('n', '[d', jump_to_prev_diagnostics_finding)
     -- Move to the next diagnostic
-    bufmap('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<cr>')
+    bufmap('n', ']d', jump_to_next_diagnostics_finding)
 
     if client.server_capabilities.documentSymbolProvider then
         navic.attach(client, bufnr)
@@ -59,6 +106,9 @@ local on_attach = function(client, bufnr)
     if client.name == 'tsserver' then
         bufmap('n', '<leader>rf', '<CMD>TypescriptRenameFile<CR>')
     end
+
+    local whichkey = require('which-key')
+    whichkey.register(lsp_mappings_whichkey, { prefix = '<leader>' })
 end
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
