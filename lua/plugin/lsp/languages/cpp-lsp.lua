2c0b2da2178496dbf68346bd8bf050296b680eaf
Kairzhan Muldashev <kair8m@gmail.com>
2023-02-24 13:59:14 +0100

    fix clang warning

--- lua/plugin/lsp/languages/cpp-lsp.lua
+++ lua/plugin/lsp/languages/cpp-lsp.lua
@@ -1,13 +1,14 @@
 local M = {}
 
 M.setup = function(lspconfig, capabilities, on_attach)
-    lspconfig['clangd'].setup {
-        init_options = {
-            clangdFileStatus = true,
-        },
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
+	capabilities.offsetEncoding = { "utf-16" }
+	lspconfig["clangd"].setup({
+		init_options = {
+			clangdFileStatus = true,
+		},
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
 end
 
 return M
