4c7d7a3203fe8eedcb33e073ac4a64e593b24377
Kairzhan Muldashev <kair8m@gmail.com>
2023-02-24 13:58:58 +0100

    add formatting keybinding

--- lua/plugin/lsp/null-ls.lua
+++ lua/plugin/lsp/null-ls.lua
@@ -1,24 +1,26 @@
-local setup, null_ls = pcall(require, 'null-ls')
+local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-    return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
 null_ls.setup({
-    sources = {
-        formatting.prettier,
-        formatting.stylua,
-        -- formatting.clang_format,
-        formatting.cmake_format,
-        formatting.autopep8,
-        formatting.shfmt,
-        formatting.beautysh,
-        formatting.rustfmt,
-        diagnostics.eslint,
-    },
-
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+		formatting.clang_format,
+		formatting.cmake_format,
+		formatting.autopep8,
+		formatting.shfmt,
+		formatting.beautysh,
+		formatting.rustfmt,
+		diagnostics.eslint,
+	},
 })
+
+local whichkey = require("which-key")
+whichkey.register({
+	l = { f = { "<cmd>lua vim.lsp.buf.format()<CR>", "Format file using lsp" } },
+}, { prefix = "<leader>" })
