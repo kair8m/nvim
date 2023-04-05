9e288be32c63521d3874a512f6145b9b8335ae51
Kairzhan Muldashev <kair@Kairzhans-MBP.fritz.box>
2023-04-05 23:32:20 +0200

    update lspsaga config

--- lua/plugin/lsp/lspsaga.lua
+++ lua/plugin/lsp/lspsaga.lua
@@ -6,9 +6,25 @@ end
 lspsaga.setup({
 	definition = {
 		edit = "<CR>",
+		vsplit = "<C-c>v",
+		split = "<C-c>i",
+		tabe = "<C-c>t",
+		quit = "q",
 	},
 	finder = {
-		edit = "<CR>",
+		max_height = 0.5,
+		min_width = 30,
+		force_max_height = false,
+		keys = {
+			jump_to = "p",
+			expand_or_jump = "<CR>",
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			tabnew = "r",
+			quit = { "q", "<ESC>" },
+			close_in_preview = "<ESC>",
+		},
 	},
 	request_timeout = 5000, -- for really large projects
 	rename = {
