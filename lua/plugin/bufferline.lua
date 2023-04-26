a6fbe510ecdb157cadc3022563e91c1b7d44a719
Kairzhan Muldashev <kair@Kairzhans-MBP.fritz.box>
2023-04-26 10:05:47 +0200

    increase distance between icon and number

--- lua/plugin/bufferline.lua
+++ lua/plugin/bufferline.lua
@@ -27,7 +27,7 @@ bufferline.setup({
 			},
 		},
 		diagnostics_indicator = function(count, level)
-			local icon = level:match("error") and "  " or " "
+			local icon = level:match("error") and "   " or "   "
 			return " " .. icon .. count
 		end,
 		separator_style = "slope",
