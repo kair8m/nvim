20d5d06c0b481f5882a2f23e3832b4bf5bff1911
Kairzhan Muldashev <kair@Kairzhans-MBP.fritz.box>
2023-04-27 19:56:57 +0200

    increase limit

--- lua/plugin/treesj.lua
+++ lua/plugin/treesj.lua
@@ -3,4 +3,8 @@ if not status then
 	return
 end
 
-treesj.setup()
+treesj.setup({
+	-- If line after join will be longer than max value,
+	-- node will not be formatted
+	max_join_length = 1000,
+})
