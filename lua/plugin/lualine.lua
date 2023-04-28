039777489939d776a77dcf9f6444d1e3ae6b60c6
Kairzhan Muldashev <kair8m@gmail.com>
2023-04-28 11:14:48 +0200

    fix typo

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -265,7 +265,7 @@ vim.api.nvim_create_autocmd("RecordingLeave", {
 		-- This is going to seem really weird!
 		-- Instead of just calling refresh we need to wait a moment because of the nature of
 		-- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
-		-- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+		-- still show a recording occurring because `vim.fn.reg_recording` hasn't emptied yet.
 		-- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
 		-- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
 		local timer = vim.loop.new_timer()
