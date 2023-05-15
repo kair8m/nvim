a22406dadf1864cddbb6044c5d9761110ce09f90
Kairzhan Muldashev <kair@Kairzhans-MBP.fritz.box>
2023-05-15 11:34:37 +0200

    change noice popup backend to cmp engine

--- lua/plugin/noice.lua
+++ lua/plugin/noice.lua
@@ -19,4 +19,8 @@ noice.setup({
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
 		lsp_doc_border = false, -- add a border to hover docs and signature help
 	},
+    popupmenu = {
+        enabled = true,
+        backend = "cmp"
+    }
 })
