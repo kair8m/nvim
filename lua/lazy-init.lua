aa8b79be21ef844b9dc7cf224f821aa3fce1517e
Kairzhan Muldashev <kair@Kairzhans-MBP.fritz.box>
2023-05-19 12:04:54 +0200

    add last place plugin
    Intelligently reopen files at your last edit position

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -206,6 +206,15 @@ require("lazy").setup({
 		"numToStr/Comment.nvim",
 		opts = {},
 	},
+
+	{
+		"ethanholz/nvim-lastplace",
+		opts = {
+			lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+			lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+			lastplace_open_folds = true,
+		},
+	},
 }, {
 	ui = {
 		border = "double",
