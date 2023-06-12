918800d48129c1ad33c12d8a24482032f3fbf0ab
Kair <kair8m@gmail.com>
2023-06-12 13:51:29 +0200

    change colorss for lualine configuration

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -6,8 +6,8 @@ end
 -- Color table for highlights
 -- stylua: ignore
 local colors = {
-    bg       = "#493E66",
-    fg       = "#bbc2cf",
+    bg       = "#7C79C3",
+    fg       = "#F1EBF6",
     yellow   = "#ECBE7B",
     cyan     = "#008080",
     darkblue = "#081633",
@@ -49,9 +49,6 @@ local config = {
 		component_separators = "",
 		section_separators = "",
 		theme = {
-			-- We are going to use lualine_c an lualine_x as left and
-			-- right section. Both are highlighted by c theme .  So we
-			-- are just setting default looks o statusline
 			normal = { c = { fg = colors.fg, bg = colors.bg } },
 			inactive = { c = { fg = colors.fg, bg = colors.bg } },
 		},
@@ -226,7 +223,7 @@ ins_right({
 ins_right({
 	"branch",
 	icon = "",
-	color = { fg = colors.violet, gui = "bold" },
+	color = { fg = colors.fg, gui = "bold" },
 })
 
 ins_right({
