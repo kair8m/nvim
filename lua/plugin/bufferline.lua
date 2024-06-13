870c546cf0f4d8525c44988349167e7e3770d90c
Kair <kair8m@gmail.com>
2024-06-13 12:18:59 +0200

    update highlight background color for selected items in bufferline
    previous one was kinda blend

--- lua/plugin/bufferline.lua
+++ lua/plugin/bufferline.lua
@@ -3,8 +3,36 @@ if not status_ok then
     return
 end
 
+local frappe = require("catppuccin.palettes").get_palette("frappe")
+local selected_style = {
+    bg = frappe.surface1,
+}
 bufferline.setup({
-    highlights = require("catppuccin.groups.integrations.bufferline").get(),
+    highlights = require("catppuccin.groups.integrations.bufferline").get({
+        styles = { "italic", "bold" },
+        custom = {
+            all = {
+                buffer_selected = selected_style,
+                close_button_selected = selected_style,
+                tab_separator_selected = selected_style,
+                diagnostic_selected = selected_style,
+                hint_selected = selected_style,
+                hint_diagnostic_selected = selected_style,
+                info_selected = selected_style,
+                info_diagnostic_selected = selected_style,
+                warning_selected = selected_style,
+                warning_diagnostic_selected = selected_style,
+                error_selected = selected_style,
+                error_diagnostic_selected = selected_style,
+                modified_selected = selected_style,
+                duplicate_selected = selected_style,
+                separator_selected = selected_style,
+                indicator_selected = selected_style,
+                pick_selected = selected_style,
+                tab_selected = selected_style,
+            },
+        },
+    }),
     options = {
         show_close_icon = false,
         diagnostics = "nvim_lsp",
