94abc209bdb5c4d9e06ea3271c77dc12d9a99179
Kair <kair8m@gmail.com>
2024-07-15 14:52:40 +0200

    fix dap config

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -81,7 +81,10 @@ require("lazy").setup({
 
     { "mfussenegger/nvim-dap" },
     { "folke/neodev.nvim" },
-    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    },
     { "theHamsta/nvim-dap-virtual-text" },
 
     { "nvim-telescope/telescope-dap.nvim" },
