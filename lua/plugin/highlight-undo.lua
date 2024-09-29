04de90afa28b14b599bdb64111d4cbcab1b78b61
Kair <kair8m@gmail.com>
2024-09-29 23:54:13 +0200

    fix highlight undo

--- lua/plugin/highlight-undo.lua
+++ lua/plugin/highlight-undo.lua
@@ -3,10 +3,19 @@ if not status then
     return
 end
 highlight_undo.setup({
-    hlgroup = "HighlightUndo",
-    duration = 500,
-    keymaps = {
-        { "n", "u", "undo", {} },
-        { "n", "<C-r>", "redo", {} },
+  duration = 300,
+  keymaps = {
+    Keymap_name = {
+      -- most fields here are the same as in vim.keymap.set
+      desc = "a description",
+      hlgroup = 'HighlightUndo',
+      mode = 'n',
+      lhs = 'lhs',
+      rhs = 'optional, can be nil',
+      opts = {
+        -- same as opts to vim.keymap.set. if rhs is nil, there should be a
+        -- callback key which points to a function
+      },
     },
+  },
 })
