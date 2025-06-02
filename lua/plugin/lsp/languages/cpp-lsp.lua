e8897b43ae0ffb1a2bfbd6cdaf17323751f97b81
Kair <kair8m@gmail.com>
2025-06-02 11:18:30 -0700

    restore switch source header functionality for `clangd`

--- lua/plugin/lsp/languages/cpp-lsp.lua
+++ lua/plugin/lsp/languages/cpp-lsp.lua
@@ -2,6 +2,27 @@ local M = {}
 
 local clangd_path = vim.fn.stdpath("data") .. "/mason/bin/clangd"
 
+local function switch_source_header(bufnr)
+    local method_name = "textDocument/switchSourceHeader"
+    local client = vim.lsp.get_clients({ bufnr = bufnr, name = "clangd" })[1]
+    if not client then
+        return vim.notify(
+            ("method %s is not supported by any servers active on the current buffer"):format(method_name)
+        )
+    end
+    local params = vim.lsp.util.make_text_document_params(bufnr)
+    client:request(method_name, params, function(err, result)
+        if err then
+            error(tostring(err))
+        end
+        if not result then
+            vim.notify("corresponding file cannot be determined")
+            return
+        end
+        vim.cmd.edit(vim.uri_to_fname(result))
+    end, bufnr)
+end
+
 M.setup = function(capabilities, on_attach)
     capabilities.offsetEncoding = { "utf-16" }
     vim.lsp.config.clangd = {
@@ -27,7 +48,12 @@ M.setup = function(capabilities, on_attach)
             semanticHighlighting = true,
             inlayHints = true,
         },
-        on_attach = on_attach,
+        on_attach = function(client, bufnr)
+            on_attach(client, bufnr)
+            vim.api.nvim_buf_create_user_command(bufnr, "ClangdSwitchSourceHeader", function()
+                switch_source_header(bufnr)
+            end, { desc = "Switch between source/header" })
+        end,
     }
     vim.lsp.enable("clangd")
 end
