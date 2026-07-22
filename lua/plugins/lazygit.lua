7024640d135717dea4f9c8097f3220fd1de9213f
Kair <kair8m@gmail.com>
2026-07-21 22:25:27 -0700

    add deps for lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,6 +1,16 @@
 return {
     "kdheepak/lazygit.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
     keys = {
         { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Toggle lazygit" },
+    },
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
     },
 }
